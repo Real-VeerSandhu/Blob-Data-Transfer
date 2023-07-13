84b01609fe2bba2d12c26febfccf422fceae0608
Veer Sandhu <sandhu.s.veer@gmail.com>
2023-07-13 10:07:42 -0400

    fix import

--- read.ts
+++ read.ts
@@ -1,4 +1,4 @@
-import * as fs from 'fs';
+const fs = require('fs'); // import module was an error
 
 const jsonString = fs.readFileSync('./data/sample-response.json', 'utf-8');
 const jsonData = JSON.parse(jsonString);
@@ -7,8 +7,8 @@ console.log(jsonData);
 
 console.log(typeof jsonData)
 
-// const str = JSON.stringify(jsonData);
-// const bytes = new TextEncoder().encode(str);
+const str = JSON.stringify(jsonData);
+const bytes = new TextEncoder().encode(str);
 // const blob = new Blob([bytes], {
 //     type: "application/json;charset=utf-8"
 // });
