0a3cb84595b61e9d57230abd052d91b33eb788a8
Veer Sandhu <sandhu.s.veer@gmail.com>
2023-07-13 10:08:58 -0400

    fix bytes error

--- read.ts
+++ read.ts
@@ -3,12 +3,13 @@ const fs = require('fs'); // import module was an error
 const jsonString = fs.readFileSync('./data/sample-response.json', 'utf-8');
 const jsonData = JSON.parse(jsonString);
 
-console.log(jsonData);
-
-console.log(typeof jsonData)
+// console.log(jsonData);
+// console.log(typeof jsonData);
 
 const str = JSON.stringify(jsonData);
 const bytes = new TextEncoder().encode(str);
+
+
 // const blob = new Blob([bytes], {
 //     type: "application/json;charset=utf-8"
 // });
