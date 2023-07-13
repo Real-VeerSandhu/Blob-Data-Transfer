879c58638ab5aa0c9c2f7f665756e69bba6ff70c
Veer Sandhu <sandhu.s.veer@gmail.com>
2023-07-13 10:14:25 -0400

    fix blob read

--- read.ts
+++ read.ts
@@ -1,3 +1,4 @@
+const Blob = require('node:buffer');
 const fs = require('fs'); // import module was an error
 
 const jsonString = fs.readFileSync('./data/sample-response.json', 'utf-8');
@@ -9,17 +10,19 @@ const jsonData = JSON.parse(jsonString);
 const str = JSON.stringify(jsonData);
 const bytes = new TextEncoder().encode(str);
 
+console.log('Bytes:', bytes);
 
-// const blob = new Blob([bytes], {
-//     type: "application/json;charset=utf-8"
-// });
+
+const blob = new Blob([bytes], {
+    type: "application/json;charset=utf-8"
+});
 
 // for (let i of str) {
 //     console.log(i)
 // }
 
 
-// console.log('blob:', blob)
+console.log('blob:', blob)
 
 /**
  * 
