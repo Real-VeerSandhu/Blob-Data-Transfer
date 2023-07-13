75d165ffb809851dded7a788f7c2d910598b74af
Veer Sandhu <sandhu.s.veer@gmail.com>
2023-07-13 10:32:03 -0400

    fix fs import

---
+++ load-data.js
@@ -0,0 +1,25 @@
+import {Blob} from 'node:buffer';
+import * as fs from 'fs';
+
+const jsonString = fs.readFileSync('./data/sample-response.json', 'utf-8');
+const jsonData = JSON.parse(jsonString);
+
+// console.log(jsonData);
+// console.log(typeof jsonData);
+
+const str = JSON.stringify(jsonData);
+const bytes = new TextEncoder().encode(str);
+
+console.log('Bytes:', bytes);
+
+
+const blob = new Blob([bytes], {
+    type: "application/json;charset=utf-8"
+});
+
+// for (let i of str) {
+//     console.log(i)
+// }
+
+
+console.log('blob:', blob)
